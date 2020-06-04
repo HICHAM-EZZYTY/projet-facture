6ccdcb31972b4f75fb1b839ea3da0528bcb6782a
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-06-04 23:37:32 +0100

    ImportingTheFontsNeeded

--- src/components/Devis/TheExportDevis.vue
+++ src/components/Devis/TheExportDevis.vue
@@ -1,7 +1,6 @@
 <template>
 
 <p>This is testing right now</p>
-  
 
 </template>
 
@@ -29,12 +28,16 @@ export default {
 }
 </script>
 
-<style >
+<style  lang="scss" >
+
+@import "../../scss/main.scss" ;   
+
+
 
 p{
-    font-family:var(--f-sb);
-    color: var(--c-r);
-    background-color: var(--c-r);
+    color: $primary;
+    background-color:$primary;
+
 }
 
 </style>

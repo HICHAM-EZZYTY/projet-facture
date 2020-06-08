dcb588ec1f4926b147b8eb8bf171810d0132b1b4
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-06-08 16:37:34 +0100

    AddEmptyCard

--- src/components/Devis/TheRefusDevis.vue
+++ src/components/Devis/TheRefusDevis.vue
@@ -1,11 +1,12 @@
 <template>
 
 <div class="containerGlobal">
-
-<div class="rec1">
+  <div class="rec1">
   <img src="../../assets/img/Group 34.svg" alt="PictureOfRectange1">
+  </div>
   <div class="circle"></div>
-</div>
+  <div id="card"></div>
+
 
 
 
@@ -29,7 +30,7 @@ export default {
     overflow-x: hidden;
 }
 .rec1{
-  background-image: url("../../assets/img/Rectangle 45.svg");
+   background-image: url("../../assets/img/Rectangle 45.svg");
     background-color: transparent;
     background-position: center;
     background-repeat: no-repeat;
@@ -38,9 +39,11 @@ export default {
     height: 183px;
     width: 331px;
     border-radius: 6px;
-    top: 20%;
+    top: 14%;
     left: 18.2%;
     z-index: 1;
+    float:left;
+
     img{
     height: 128px;
     position: relative;
@@ -48,6 +51,31 @@ export default {
     left: 13%;
     }
 }
+#card{
 
+    background-color: $blue;
+    height: 288px;
+    width: 665px;
+    border-radius: 10px;
+    z-index: 2;
+    position: relative;
+    left: 24%;
+    top: 17%;
+
+}
+.circle {
+    background-image: url("../../assets/img/Ellipse 41.svg");
+    background-color: transparent;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    height: 93px;
+    width: 93px;
+    z-index: 1;
+    float: right;
+    top: 96px;
+    right: 29%;
+    }
 
 </style>

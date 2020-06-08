3567847f6db99addb3f49bc931e3dbf6dee5d19c
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-06-08 17:36:20 +0100

    cardContentoftherefusdeviscompo

--- src/components/Devis/TheRefusDevis.vue
+++ src/components/Devis/TheRefusDevis.vue
@@ -1,13 +1,32 @@
 <template>
 
 <div class="containerGlobal">
+
+<!-- Starting of the layout  -->
+
   <div class="rec1">
   <img src="../../assets/img/Group 34.svg" alt="PictureOfRectange1">
   </div>
   <div class="circle"></div>
-  <div id="card"></div>
+  <div id="card">
+
+      <div class="header">
+          <!-- <div id="draw1"> -->
+                <img  id="draw1" src="../../assets/img/path.svg" alt="DrawingOfapath">
+          <!-- </div> -->
+        <h1>Hé, attends !!</h1>
+          <!-- <div id="draw2"> -->
+                <img id="draw2" src="../../assets/img/path (1).svg" alt="DrawingOfapath">
+          <!-- </div> -->
+      </div>
+
+
+  </div>
   <div class="rec2"></div>
   <div class="circleclose"></div>
+
+<!-- Ending of the layout  -->
+
 
 
 
@@ -25,6 +44,10 @@ export default {
 
 <style scoped lang="scss" >
 @import "../../scss/main.scss" ;   
+
+
+// Starting of the layout Styling
+
 .containerGlobal {
     background-color: $background;
     height: 615px;
@@ -108,6 +131,34 @@ export default {
     height: 192px;
     width: 173px;
     }
+  // Ending of the layout Styling 
+  .header{
+    display:grid;
+    grid-template-columns: 2fr 1fr 2fr;
+    position: absolute;
+    width: 100%;
+    h1{
+      font-family:  $bd;
+      font-size: 17px;
+      color: #FFFFFF;
+      margin-top: 113px;
+      justify-self: center;
+    }
+  }
+
+  #draw1{
+    
+    justify-self: flex-end;
+    height: 78px;
+    margin-top: 55px;
+  }
+  #draw2{
+    
+    justify-self: flex-start;
+    height: 78px;
+    margin-top: 55px;
+  }
+
     // Start the responsive web 
     @media (max-width: 1000px) {
 
@@ -159,7 +210,6 @@ export default {
         width: 85%;
       }
     }
-
         @media (max-width: 450px) {
            .rec1{
         img{
@@ -167,6 +217,8 @@ export default {
         }
       }
         }
+
+        // Ending of responsive layout 
 
 
 

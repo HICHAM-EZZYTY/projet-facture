94b3eb1d597f9e2e3801ff70b6b2c098275e7f57
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-06-16 19:59:57 +0100

    AddingTheStlingScrollBarToTheClientsCompo

--- src/components/Clients-page/cards.vue
+++ src/components/Clients-page/cards.vue
@@ -43,10 +43,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	::-webkit-scrollbar { 
-		display: none; 
+	::-webkit-scrollbar {
+	width: 20px;
 	}
-
+	::-webkit-scrollbar-track {
+	box-shadow: inset 0 0 5px grey; 
+	border-radius: 10px;
+	}
+	::-webkit-scrollbar-thumb {
+	background: red; 
+	border-radius: 10px;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #b30000; 
+}
+	
 	.container {
 		display: flex;
 		flex-wrap: wrap;
